3afcbe43423a24812d5d46e85d260ba1dc8b8864
alor <alberto.ornaghi@gmail.com>
2024-05-13 14:43:29 +0200

    deploy to vercel

--- src/app/frames/home.tsx
+++ src/app/frames/home.tsx
@@ -6,7 +6,7 @@ export default function FrameHome() {
         <FrameTemplate>
             <FrameLogo/>
             <h1 tw="text-[60px] text-white font-bold text-left">Miles O&apos;Meter</h1>
-            <div tw="flex flex-col w-full text-white text-end items-end mt-8">
+            <div tw="flex flex-col w-full text-white items-end mt-8">
                 <h5>Race time calculator and Pace converter metric/imperial</h5>
             </div>
         </FrameTemplate>
